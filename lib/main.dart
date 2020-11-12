aab2191e7fa7591ae1921de8f60c95e9f9ebc7a2
ekyeremeh7 <ekyeremeh7@gmail.com>
2020-11-12 08:02:06 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
+// import 'package:flutter_sequence_animation/flutter_sequence_animation.dart';
+// import 'package:rubber/rubber.dart';
+// import 'package:sprung/sprung.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:animated_text_kit/animated_text_kit.dart';
+// import 'package:modal_progress_hud/modal_progress_hud.dart';
+
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+//      home: WelcomeScreen(),
+//      initialRoute: '/',
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+      },
+    );
+  }
+}
